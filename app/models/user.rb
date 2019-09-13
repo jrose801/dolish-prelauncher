6a3395d64c08d4585efa0fbd4fcd871cbaf401fb
jrose801 <jrose801@gmail.com>
2019-09-13 17:16:10 -0600

    add line to users.rb to fix download csv file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class User < ActiveRecord::Base
 	belongs_to :referrer, class_name: "User", foreign_key: :referrer_id
     has_many :referrals, class_name: "User", foreign_key: :referrer_id
